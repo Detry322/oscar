07b7d0230c7d1b3b20f9a0ce221394df8ba31a7a
Anish Athalye <me@anishathalye.com>
2015-07-01 09:01:59 -0700

    Revert "Make backoff ignore successful results"
    This reverts commit 84211ca3b49c187736efea8fc1bb83644931bc31.

--- src/main/scala/com/anishathalye/oscar/reporter/BackoffReporter.scala
+++ src/main/scala/com/anishathalye/oscar/reporter/BackoffReporter.scala
@@ -1,6 +1,6 @@
 package com.anishathalye.oscar.reporter
 
-import com.anishathalye.oscar.{ Result, Success }
+import com.anishathalye.oscar.Result
 
 import concurrent.duration._
 import collection.mutable.{ Map => MMap, HashMap }
@@ -38,38 +38,33 @@ case class BackoffReporter(reporter: Reporter, low: Duration, high: Duration) ex
   }
 
   override def apply(name: String, result: Result) {
-    result match {
-      case Success => reporter(name, result) // no rate limiting
-      case _ => {
-        val now = new Date()
-        getLast(name) flatMap { last =>
-          getTimeout(name) map { timeout =>
-            (last, timeout)
-          }
-        } match {
-          case None => {
-            setLast(name, now)
-            setTimeout(name, low)
-            reporter(name, result)
-          }
-          case Some((last, timeout)) => {
-            // check if we need to do exponential backoff
-            val diff = now.getTime - last.getTime
-            val threshold = timeout.toMillis
-            if (diff < threshold) {
-              // suppress, hasn't been long enough
-            } else if (diff < threshold * FACTOR) {
-              // send it, double threshold
-              setLast(name, now)
-              setTimeout(name, min(threshold * FACTOR, high.toMillis).milliseconds)
-              reporter(name, result)
-            } else {
-              // send it, shrink threshold
-              setLast(name, now)
-              setTimeout(name, max(threshold / FACTOR, low.toMillis).milliseconds)
-              reporter(name, result)
-            }
-          }
+    val now = new Date()
+    getLast(name) flatMap { last =>
+      getTimeout(name) map { timeout =>
+        (last, timeout)
+      }
+    } match {
+      case None => {
+        setLast(name, now)
+        setTimeout(name, low)
+        reporter(name, result)
+      }
+      case Some((last, timeout)) => {
+        // check if we need to do exponential backoff
+        val diff = now.getTime - last.getTime
+        val threshold = timeout.toMillis
+        if (diff < threshold) {
+          // suppress, hasn't been long enough
+        } else if (diff < threshold * FACTOR) {
+          // send it, double threshold
+          setLast(name, now)
+          setTimeout(name, min(threshold * FACTOR, high.toMillis).milliseconds)
+          reporter(name, result)
+        } else {
+          // send it, shrink threshold
+          setLast(name, now)
+          setTimeout(name, max(threshold / FACTOR, low.toMillis).milliseconds)
+          reporter(name, result)
         }
       }
     }
