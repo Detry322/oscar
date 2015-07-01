cf06cfb9d71f91527c2e31117d3b09ad5a125850
Anish Athalye <me@anishathalye.com>
2015-07-01 11:26:45 -0700

    Add HealReporter

---
+++ src/main/scala/com/anishathalye/oscar/reporter/HealReporter.scala
@@ -0,0 +1,37 @@
+package com.anishathalye.oscar.reporter
+
+import com.anishathalye.oscar.{ Result, Success, Failure }
+
+import collection.mutable.{ Map => MMap, HashMap }
+
+case class HealReporter(reporter: Reporter) extends Reporter {
+
+  // whether last check was successful
+  val last: MMap[String, Boolean] = new HashMap[String, Boolean]()
+
+  def getLast(name: String): Boolean = this.synchronized {
+    last lift name getOrElse false
+  }
+
+  def setLast(name: String, successful: Boolean) {
+    this.synchronized {
+      last(name) = successful
+    }
+  }
+
+  override def apply(name: String, result: Result) {
+    result match {
+      case Success(report) => {
+        if (getLast(name) == false) {
+          // switched state to healed
+          reporter(name, result)
+        }
+      }
+      case Failure(report) => {
+        setLast(name, false)
+      }
+      case _ => // do nothing
+    }
+  }
+
+}
