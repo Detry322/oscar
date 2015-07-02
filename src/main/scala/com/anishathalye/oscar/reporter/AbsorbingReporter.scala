e94ce30c4c500f81f160a9a4ce4c6c14347c7b7f
Anish Athalye <me@anishathalye.com>
2015-07-01 17:28:21 -0700

    Add AbsorbingReporter

---
+++ src/main/scala/com/anishathalye/oscar/reporter/AbsorbingReporter.scala
@@ -0,0 +1,44 @@
+package com.anishathalye.oscar.reporter
+
+import com.anishathalye.oscar.{ Result, Report, Failure }
+
+import collection.mutable.{ Map => MMap, HashMap }
+
+object AbsorbingReporter {
+
+  def apply(reporter: Reporter, capacity: Int): Reporter = new Reporter {
+
+    val absorbed: MMap[String, Int] = new HashMap[String, Int]()
+
+    def getAbsorbed(name: String): Int = this.synchronized {
+      absorbed lift name getOrElse 0
+    }
+
+    def setAbsorbed(name: String, count: Int) {
+      this.synchronized {
+        absorbed(name) = count
+      }
+    }
+
+    override def apply(name: String, result: Result) {
+      result match {
+        case Failure(_) => {
+          val soFar = getAbsorbed(name)
+          if (soFar < capacity) {
+            setAbsorbed(name, soFar + 1)
+            // don't pass this on
+          } else {
+            // full
+            reporter(name, result)
+          }
+        }
+        case _ => {
+          setAbsorbed(name, 0)
+          reporter(name, result)
+        }
+      }
+    }
+
+  }
+
+}
